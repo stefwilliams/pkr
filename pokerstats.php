8cae4949e7f46bb8a41ea5b9eb3e48ac2b54d7a8
stefwilliams <stefwilliams@gmail.com>
2015-03-14 20:04:20 +0000

    Revert "first proper commit"
    This reverts commit 9533324477756cab61a781d63202e2baa3415a96.

---
+++ pokerstats.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Poker Stats
+Plugin URI: 
+Description: Keep track of poker games scores and stats
+Version: 0.1
+Author: Stef Williams
+Author Email: stefwilliams@gmail.com
+*/
+
+include_once('cpt/pkr_game.php'); // <-- the container
+include_once('cpt/pkr_venue.php');
+include_once('cpt/pkr_series.php');
+include_once('cpt/pkr_leagues.php');
+?>
